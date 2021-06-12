092d5c88e05b3ee6aa4923b09fa92b0f0b2ccbac
Neriman Öz Çetin <nozcetin@turksat.com.tr>
2021-06-12 12:09:57 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
-import App from './containers/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-reportWebVitals();
